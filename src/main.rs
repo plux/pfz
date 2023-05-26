73373d6ef7ee774ae3ec61adda6d3a041a339abd
Hakan Nilsson <haakan@gmail.com>
2023-05-26 09:51:04 +0200

    Async read and refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use core::time;
 use std::cmp::min;
 use std::io::{stdin, Stderr, self};
 use std::process::ExitCode;
+use std::time::{Duration, Instant};
 use std::{thread, fs};
 use std::io::Write;
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
@@ -10,24 +10,27 @@ use crossterm::terminal::{ClearType};
 use crossterm::{terminal, execute, cursor};
 use clap::Parser;
 use crossterm::tty::IsTty;
-use itertools::Itertools;
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    fullscreen: bool
+    fullscreen: bool,
+    #[arg(short, long, default_value_t = 10)]
+    height: usize
 }
 
 struct FuzzyMatcher {
-    cursor: usize,
-    height: usize,
     items: Vec<String>,
-    matches: Vec<String>,
-    offset: usize,
     query: Query,
     outstream: Stderr,
-    args: Args
+    args: Args,
+    match_list: MatchList,
+    items_receiver: Receiver<String>,
+    last_render: Instant,
+    need_update: bool
 }
 
 enum HandleEventResult {
@@ -37,47 +40,134 @@ enum HandleEventResult {
     Continue,
 }
 
+struct MatchList {
+    pub height: usize,
+    cursor: usize,
+    offset: usize,
+    pub matches: Vec<String>
+}
+
+impl MatchList {
+    fn new(height: usize) -> Self {
+        Self{height, cursor: 0, offset: 0, matches: Vec::new()}
+    }
+
+    fn get_selection(&self) -> &String {
+        &self.matches[self.cursor + self.offset]
+    }
+
+    fn move_up(&mut self) {
+        if self.cursor > 0 {
+            self.cursor -= 1
+        } else if self.offset > 0 {
+            self.offset -= 1
+        }
+    }
+
+    fn move_down(&mut self) {
+        if self.cursor < self.height - 1 {
+            self.cursor += 1
+        } else if self.offset < self.matches.len() - self.height {
+            self.offset += 1
+        }
+    }
+
+    fn adjust_cursor(&mut self) {
+        if self.matches.len() > 0 && self.cursor >= self.matches.len() {
+            self.cursor = self.matches.len() - 1
+        }
+    }
+
+    fn adjust_offset(&mut self) {
+        if self.offset > self.matches.len() {
+            self.offset = 0;
+        }
+    }
+
+    fn print(&mut self, mut outstream: &Stderr, query: &Query) {
+        self.adjust_cursor();
+        self.adjust_offset();
+        for i in 0..self.height {
+            if let Some(m) = self.matches.get(i + self.offset) {
+                let (cols, _rows) = terminal::size().unwrap();
+                let w: usize = min((cols - 10).into(), m.len());
+                if self.cursor == i {
+                    execute!(outstream, Print(">".red()),
+                             SetAttribute(Attribute::Bold)).unwrap();
+                } else {
+                    write!(outstream, " ").unwrap();
+                }
+                let mut match_str = m.to_string()[..w].to_string();
+                for query_part in &query.query {
+                    if let Some(begin) = match_str.find(query_part) {
+                        let end = begin+query_part.len();
+                        match_str = format!("{}{}{}",
+                                            &match_str[..begin],
+                                            &match_str[begin..end].dark_cyan(),
+                                            &match_str[end..]
+                        );
+                    }
+                }
+                write!(outstream, " {} {}\n\r",
+                       i + self.offset,
+                       &match_str,
+                ).unwrap();
+
+                execute!(outstream,
+                         SetAttribute(Attribute::Reset)).unwrap();
+            } else {
+                execute!(outstream,
+                         terminal::Clear(ClearType::CurrentLine),
+                         Print("\n\r")).unwrap();
+            }
+        }
+    }
+}
+
 impl FuzzyMatcher {
     fn new(args: Args) -> Self {
         terminal::enable_raw_mode().unwrap();
         let mut stderr = io::stderr();
-        let height: usize = if args.fullscreen {
-            // Set height to the height of the terminal
-            terminal::size().unwrap().1.checked_sub(1).unwrap().try_into().unwrap()
-        } else {
-            // TODO: Allow reading height from args
-            10
-        };
+        let term_height: usize = terminal::size().unwrap().1.checked_sub(1)
+            .unwrap().try_into().unwrap();
+        let mut height: usize = min(args.height, term_height);
         if args.fullscreen {
+            // Set height to the height of the terminal
+            height = term_height;
             execute!(stderr, terminal::EnterAlternateScreen).unwrap();
         }
         execute!(stderr, cursor::Hide).unwrap();
+        let (items_sender, items_receiver): (Sender<String>, Receiver<String>) =
+            mpsc::channel();
+        thread::spawn(move || {
+            FuzzyMatcher::read_input(items_sender);
+        });
         Self {
             args,
-            cursor: 0,
-            height,
+            match_list: MatchList::new(height),
             items: Vec::new(),
-            matches: Vec::new(),
-            offset: 0,
             query: Query::new(String::new()),
-            outstream: stderr
+            outstream: stderr,
+            items_receiver,
+            last_render: Instant::now(),
+            need_update: true
         }
     }
 
-    fn read_input(&mut self) {
+    fn read_input(items_sender: Sender<String>) {
         //TODO: Read async
         if stdin().is_tty() {
             //push every filename in current dirlisting to self.items
             for entry in fs::read_dir(".").unwrap() {
                 let filename = entry.unwrap().file_name();
                 let filename_str = filename.to_string_lossy();
-                self.items.push(filename_str.into_owned());
+                items_sender.send(filename_str.into_owned()).unwrap();
             }
             return
         }
         for line in stdin().lines() {
             let l = line.expect("not a line?");
-            self.items.push(l);
+            items_sender.send(l).unwrap();
         }
     }
 
@@ -85,31 +175,19 @@ impl FuzzyMatcher {
         if let Ok(Event::Key(KeyEvent{code, modifiers, ..})) = event {
             match (code, modifiers) {
                 (KeyCode::Enter, _) =>
-                    if self.matches.len() == 0 {
+                    if self.match_list.matches.len() == 0 {
                         return HandleEventResult::NoMatch
                     } else {
                         return HandleEventResult::Done
                     }
-                (code, modifiers)
-                    if modifiers == &KeyModifiers::CONTROL
-                    || code == &KeyCode::Up || code == &KeyCode::Down
-                    => {
-                    // TODO: Add support for arrows in a nicer way
-                    match code {
-                        KeyCode::Char('c') =>
-                            return HandleEventResult::Quit,
-                        KeyCode::Char('p') | KeyCode::Up if self.cursor > 0 =>
-                            self.cursor -= 1,
-                        KeyCode::Char('p') | KeyCode::Up if self.offset > 0 =>
-                            self.offset -= 1,
-                        KeyCode::Char('n') | KeyCode::Down if self.cursor < self.height - 1 =>
-                            self.cursor += 1,
-                        KeyCode::Char('n') | KeyCode::Down
-                            if self.offset < self.matches.len() - self.height =>
-                            self.offset += 1,
-                        _ => ()
-                    }
-                }
+                (KeyCode::Char('c'), &KeyModifiers::CONTROL) =>
+                    return HandleEventResult::Quit,
+                (KeyCode::Char('p'), &KeyModifiers::CONTROL) =>
+                    self.match_list.move_up(),
+                (KeyCode::Char('n'), &KeyModifiers::CONTROL) =>
+                    self.match_list.move_down(),
+                (KeyCode::Up, _) => self.match_list.move_up(),
+                (KeyCode::Down, _) => self.match_list.move_down(),
                 (KeyCode::Char(c), _) => {
                     // TODO: Move inside Query
                     let mut query_str = self.query.query_str.to_string();
@@ -128,7 +206,7 @@ impl FuzzyMatcher {
     }
 
     fn clear_lines(&mut self) {
-        for _ in 0..self.height {
+        for _ in 0..self.match_list.height {
             execute!(self.outstream,
                      terminal::Clear(ClearType::CurrentLine),
                      Print("\n\r")
@@ -154,107 +232,70 @@ impl FuzzyMatcher {
             Print(format!("> {}", self.query.query_str)),
             Print(" ".negative()),
 
-            Print(format!(" [{}/{}]", self.matches.len(), self.items.len()).dim())
+            Print(format!(" [{}/{}]", self.match_list.matches.len(), self.items.len()).dim())
         )
         .unwrap();
     }
 
-    fn print_matches(&mut self) {
-        for i in 0..self.height {
-            if let Some(m) = self.matches.get(i + self.offset) {
-                // TODO: Hilight matching text
-                let (cols, _rows) = terminal::size().unwrap();
-                let w: usize = min((cols - 10).into(), m.len());
-                if self.cursor == i {
-                    execute!(self.outstream, Print(">".red()),
-                             SetAttribute(Attribute::Bold)).unwrap();
-                } else {
-                    write!(self.outstream, " ").unwrap();
-                }
-                let mut match_str = m.to_string()[..w].to_string();
-                for query_part in &self.query.query {
-                    if let Some(begin) = match_str.find(query_part) {
-                        let end = begin+query_part.len();
-                        match_str = format!("{}{}{}",
-                                            &match_str[..begin],
-                                            &match_str[begin..end].dark_cyan(),
-                                            &match_str[end..]
-                        );
-                    }
-                }
-
-                write!(self.outstream,
-                       " {} {}\n\r",
-                       i + self.offset,
-                       &match_str,
-                       ).unwrap();
-
-                execute!(self.outstream,
-                         SetAttribute(Attribute::Reset)).unwrap();
-            } else {
-                execute!(self.outstream,
-                         terminal::Clear(ClearType::CurrentLine),
-                         Print("\n\r")).unwrap();
-            }
-        }
-    }
 
     fn move_cursor_to_top(&mut self) {
         execute!(self.outstream,
-                 cursor::MoveUp((self.height).try_into().unwrap())
+                 cursor::MoveUp((self.match_list.height).try_into().unwrap())
         ).unwrap();
     }
 
-    fn adjust_cursor(&mut self) {
-        if self.matches.len() > 0 && self.cursor >= self.matches.len() {
-            self.cursor = self.matches.len() - 1
-        }
-    }
-
-    fn adjust_offset(&mut self) {
-        if self.offset > self.matches.len() {
-            self.offset = 0;
-        }
-    }
-
     fn render(&mut self) {
-        self.clear_lines();
-        self.adjust_cursor();
-        self.adjust_offset();
-        self.print_matches();
-        self.print_prompt();
-        self.move_cursor_to_top();
+        if self.last_render.elapsed().as_millis() > 10 {
+            self.clear_lines();
+            self.match_list.print(&self.outstream, &self.query);
+            self.print_prompt();
+            self.move_cursor_to_top();
+            self.last_render = Instant::now();
+            self.need_update = false;
+        }
     }
 
     fn main(&mut self) -> ExitCode {
-
         // TODO: Move main loop
         loop {
-            self.matches = self.find_matches();
-            self.render();
-            let ten_millis = time::Duration::from_millis(1);
-            thread::sleep(ten_millis);
-            // TODO: Figure out how to receive windows resize event
-            let event = crossterm::event::read();
-            match self.handle_event(&event) {
-                HandleEventResult::Done => {
-                    self.restore_terminal();
-                    execute!(self.outstream,
-                             terminal::Clear(ClearType::CurrentLine)).unwrap();
-                    println!("{}",
-                             self.matches[self.cursor + self.offset]);
-                    return ExitCode::SUCCESS;
+            let begin_recv = std::time::Instant::now();
+            loop {
+                if let Ok(item) = self.items_receiver.recv() {
+                    self.items.push(item);
+                    self.need_update = true;
+                } else {
+                    break;
                 }
-                HandleEventResult::NoMatch => {
-                    self.restore_terminal();
-                    return ExitCode::FAILURE
+                if begin_recv.elapsed().as_millis() > 10 {
+                    break;
                 }
-                HandleEventResult::Quit => {
-                    self.restore_terminal();
-                    return ExitCode::from(130)
-                }
-                HandleEventResult::Continue => (),
             }
+            // TODO: Figure out how to receive windows resize event
+            if let Ok(true) = crossterm::event::poll(Duration::from_micros(10)) {
+                let event = crossterm::event::read();
+                match self.handle_event(&event) {
+                    HandleEventResult::Done => {
+                        self.restore_terminal();
+                        println!("{}", self.match_list.get_selection());
+                        return ExitCode::SUCCESS;
+                    }
+                    HandleEventResult::NoMatch => {
+                        self.restore_terminal();
+                        return ExitCode::FAILURE
+                    }
+                    HandleEventResult::Quit => {
+                        self.restore_terminal();
+                        return ExitCode::from(130)
+                    }
+                    HandleEventResult::Continue =>
+                        self.need_update = true
+                }
+            }
+            if self.need_update {
+                self.match_list.matches = self.find_matches();
+                self.render();
+            }
+
         }
     }
     fn restore_terminal(&mut self) {
@@ -265,7 +306,10 @@ impl FuzzyMatcher {
                 terminal::LeaveAlternateScreen
             ).unwrap();
         }
-        // Clearing some stuff when not alternative screen is needed
+        execute!(self.outstream,
+                 terminal::Clear(ClearType::CurrentLine),
+                 cursor::MoveToColumn(0)
+        ).unwrap();
         execute!(self.outstream, cursor::Show).unwrap()
     }
 }
@@ -273,7 +317,6 @@ impl FuzzyMatcher {
 fn main() -> ExitCode {
     let args = Args::parse();
     let mut m = FuzzyMatcher::new(args);
-    m.read_input();
     m.main()
 }
 
